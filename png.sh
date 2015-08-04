77dae8b97f5fe9fb7a59a476962ab06bc5d390bc
Michael Mazzola <mimazz@microsoft.com>
2015-08-04 11:35:50 -0400

    Using 8 bit color depth on png generation

--- png.sh
+++ png.sh
@@ -4,15 +4,19 @@ function createpng {
   for d in svg/*.svg; do
     png=$(echo "$d" | sed 's/.svg/.png/')
     echo "creating $png ..."
-    convert -density "$2" -resize "$1" -background none "$d" "$png"
+    convert -density "$2" -resize "$1" -background none -depth 8 "$d" "$png"
   done
   mkdir -p "$1"
   mv svg/*.png "$1/"
 }
 
-createpng 20x20 160
+#inline
+#createpng 20x20 160
 #createpng 30x30 240
 #createpng 40x40 320
 #createpng 60x60 480
-#createpng 80x80 480
-#createpng 120x120 480
+#keyboard
+#createpng 32x32 160
+#createpng 48x48 240
+#createpng 64x64 320
+createpng 96x96 480
